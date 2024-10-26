f53b1a270cfd023ebad750ed42e5d2143867f33b
CzmeleN <344907@uwr.edu.pl>
2024-10-26 22:17:47 +0200

    Optimized A*Craft, achieves 10k points

--- AStarCraft/solution.cpp
+++ AStarCraft/solution.cpp
@@ -13,8 +13,8 @@ constexpr int WIDTH = 19;
 constexpr int MAX = 190;
 constexpr int DIRS = 4;
 constexpr int INTERVAL = 128;
-constexpr double LIMIT = 995.0;
-constexpr double INIT_TEMP = 50.0;
+constexpr double LIMIT = 999.0;
+constexpr double INIT_TEMP = 100000.0;
 constexpr double ALPHA = 0.99999;
 
 enum cell { VOID, PLATFORM, UP, RIGHT, DOWN, LEFT }; 
@@ -25,22 +25,24 @@ struct Uni {
     short dir;
 };
 
-static short og_board[HEIGHT][WIDTH];
-static Uni bots[HEIGHT];
-static int bot_count = 0;
-
 class State{
 private:
     static std::mt19937 generator;
-    short board[HEIGHT][WIDTH];
+    std::chrono::time_point<std::chrono::high_resolution_clock> start;
+    std::pair<short, short> possibs[MAX];
+    short og_board[HEIGHT][WIDTH];
+    short curr_board[HEIGHT][WIDTH];
+    short best_board[HEIGHT][WIDTH];
+    Uni bots[HEIGHT];
+    int bot_count = 0, possibs_count = 0;
 
-    std::vector<Uni> get_new_arrows(State *orig) {
+    std::vector<Uni> get_new_arrows() {
         std::vector<Uni> res;
 
         for (short i = 0; i < HEIGHT; ++i) {
             for (short j = 0; j < WIDTH; ++j) {
-                if (board[i][j] >= UP && orig->board[i][j] < UP) {
-                    res.push_back(Uni{j, i, board[i][j]});
+                if (best_board[i][j] >= UP && og_board[i][j] < UP) {
+                    res.push_back(Uni{j, i, best_board[i][j]});
                 }
             }
         }
@@ -70,32 +72,56 @@ private:
 
     bool pointless(short x, short y) {
         bool voids[DIRS];
+        bool not_platforms[DIRS];
         short curr = og_board[x == 0 ? HEIGHT - 1 : x - 1][y];
         
         if (curr >= UP) return false;
         voids[0] = curr == VOID;
+        curr = og_board[x <= 1 ? HEIGHT - 2 : x - 2][y];
+        not_platforms[0] = curr != PLATFORM;
 
         curr = og_board[x][(y + 1) % WIDTH];
         if (curr >= UP) return false;
         voids[1] = curr == VOID;
+        curr = og_board[x][(y + 2) % WIDTH];
+        not_platforms[1] = curr != PLATFORM;
 
         curr = og_board[(x + 1) % HEIGHT][y];
         if (curr >= UP) return false;
         voids[2] = curr == VOID; 
+        curr = og_board[(x + 2) % HEIGHT][y];
+        not_platforms[2] = curr != PLATFORM;
 
         curr = og_board[x][y == 0 ? WIDTH - 1 : y - 1];
         if (curr >= UP) return false;
         voids[3] = curr == VOID;
+        curr = og_board[x][y <= 1 ? WIDTH - 2 : y - 2];
+        not_platforms[3] = curr != PLATFORM;
 
+        // tunnel
         if ((voids[0] && voids[2] && !voids[1] && !voids[3]) || (voids[1] && voids[3] && !voids[0] && !voids[2])) {
             return true;
         }
 
-        if (!voids[0] && !voids[1] && !voids[2] && !voids[3]) {
-            return og_board[(x + 1) % HEIGHT][(y + 1) % WIDTH] == PLATFORM;
+        if (!voids[0] && !voids[1] && !voids[2] && !voids[3] && og_board[(x + 1) % HEIGHT][(y + 1) % WIDTH] == VOID) {
+            return false; 
         }
 
-        return false;
+        if (voids[0] || voids[1] || voids[2] || voids[3] || not_platforms[0] || not_platforms[1] || not_platforms[2] || not_platforms[3]) {
+            return false;
+        }
+
+        return true;
+    }
+
+    void preprocess() {
+        for (int i = 0; i < HEIGHT; ++i) {
+            for (int j = 0; j < WIDTH; ++j) {
+                if (og_board[i][j] == PLATFORM && !pointless(i, j)) {
+                    possibs[possibs_count++] = {i, j};
+                }
+            }
+        }
     }
 
     void generate_random() {
@@ -107,7 +133,7 @@ private:
 
         for (short i = 0; i < HEIGHT; ++i) {
             for (short j = 0; j < WIDTH; ++j) {
-                if (board[i][j] == PLATFORM && !pointless(i, j)) {
+                if (curr_board[i][j] == PLATFORM && !pointless(i, j)) {
                     possibs.push_back({i, j});
                 }
             }
@@ -128,60 +154,65 @@ private:
                 nx = x == 0 && dx[i] == -1 ? HEIGHT - 1 : (x + dx[i]) % HEIGHT;
                 ny = y == 0 && dy[i] == -1 ? WIDTH - 1 : (y + dy[i]) % WIDTH;
 
-                if (board[nx][ny] != VOID && board[nx][ny] != (i + 2) % DIRS + 2) {
+                if (curr_board[nx][ny] != VOID && curr_board[nx][ny] != (i + 2) % DIRS + 2) {
                     dirs[it++] = i + 2;
                 }
             }
 
-            board[x][y] = dirs[random(0, it)];
+            curr_board[x][y] = dirs[random(0, it)];
         }
     }
 
-    void make_change() {
-        std::pair<short, short> possibs[MAX];
+    Uni make_change() {
         constexpr short dx[] = {-1, 0, 1, 0}, dy[] = {0, 1, 0, -1};
-        int x, y, nx, ny;
-        unsigned int it = 0, pick;
-        short dirs[5] = {PLATFORM}, op;
+        Uni res;
+        short x, y, nx, ny, curr;
+        unsigned int it = 0, new_it, pick;
+        short dirs[5], op;
 
-        for (int i = 0; i < HEIGHT; ++i) {
-            for (int j = 0; j < WIDTH; ++j) {
-                if (og_board[i][j] == PLATFORM && !pointless(i, j)) {
-                    possibs[it++] = {i, j};
-                }
-            }
-        }
-
-        pick = xorshift_rand() % it;
-        it = 1;
+        pick = xorshift_rand() % possibs_count;
         x = possibs[pick].first;
         y = possibs[pick].second;
-        
+        curr = curr_board[x][y];
+
+        if (curr != PLATFORM) {
+            dirs[0] = PLATFORM;
+            it = 1;
+        }
+        else {
+            it = 0;
+        }
+
         for (short i = 0; i < 4; ++i) {
+            if (curr == i + 2) continue;
 
             nx = x == 0 && dx[i] == -1 ? HEIGHT - 1 : (x + dx[i]) % HEIGHT;
             ny = y == 0 && dy[i] == -1 ? WIDTH - 1 : (y + dy[i]) % WIDTH;
 
-            if (board[nx][ny] != VOID && board[nx][ny] != (it + 2) % 4 + 2) {
+            if (curr_board[nx][ny] != VOID) {
                 dirs[it++] = i + 2;
             }
         }
 
-        int new_it = xorshift_rand() % it;
-        board[x][y] = dirs[new_it];
+        new_it = xorshift_rand() % it;
+        res = {x, y, curr_board[x][y]};
+        curr_board[x][y] = dirs[new_it];
+
+        return res;
     }
 
     int simulate() {
         int res = 0;
-        short x, y, dir;
+        short x, y, dir, bot_mult;
         uint64_t visited[HEIGHT][WIDTH] = {};
 
         for (int i = 0; i < bot_count; ++i) {
+            bot_mult = i * 4;
             x = bots[i].x;
             y = bots[i].y;
-            dir = board[y][x] >= UP ? board[y][x] : bots[i].dir;
+            dir = curr_board[y][x] >= UP ? curr_board[y][x] : bots[i].dir;
 
-            visited[y][x] |= 1 << (dir - 2 + i * 4);
+            visited[y][x] |= (1 << (dir - 2 + bot_mult));
 
             while(true) {
                 res++;
@@ -197,23 +228,23 @@ private:
                         ++y %= HEIGHT;
                         break;
                     case LEFT:
-                        x = x == 0 ? HEIGHT - 1 : x - 1;
+                        x = x == 0 ? WIDTH - 1 : x - 1;
                         break;
                 }
 
-                if (board[y][x] == VOID) {
+                if (curr_board[y][x] == VOID) {
                     break;
                 }
 
-                if (board[y][x] >= UP) {
-                    dir = board[y][x];
+                if (curr_board[y][x] >= UP) {
+                    dir = curr_board[y][x];
                 }
 
-                if (visited[y][x] & 1 << (dir - 2 + i * 4)) {
+                if (visited[y][x] & (1 << (dir - 2 + bot_mult))) {
                     break;
                 }
 
-                visited[y][x] |= 1 << (dir - 2 + i * 4);
+                visited[y][x] |= (1 << (dir - 2 + bot_mult));
             }
         }
 
@@ -224,17 +255,15 @@ private:
         State best;
         int curr, max = 0, count = 0;
         double time_left = 1000.0;
-        auto start = std::chrono::high_resolution_clock::now();
 
         while (time_left > 10) {
             // zmienic na co np64
-            State new_state(this);
-            new_state.generate_random();
-            curr = new_state.simulate();
+            generate_random();
+            curr = simulate();
 
             if (curr > max) {
                 max = curr;
-                best = new_state;
+                update_best();
             }
 
             auto time = std::chrono::high_resolution_clock::now();
@@ -244,33 +273,44 @@ private:
         }
 
         // std::cerr << "simulations: " << count << " max score: " << max << std::endl;
-        return best.get_new_arrows(this);
+        return best.get_new_arrows();
+    }
+
+    void update_best() {
+        std::memcpy(best_board, curr_board, sizeof(curr_board));
+        // for (int i = 0; i < HEIGHT; ++i) {
+        //     for (int j = 0; j < WIDTH; ++j) {
+        //         std::cerr << best_board[i][j] << ' ';
+        //     }
+        //     std::cerr << std::endl;
+        // }
+
     }
 
     std::vector<Uni> best_sa() {
-        State curr = *this;
-        State best = curr;
-        int curr_score = curr.simulate();
-        int best_score = curr_score, delta, neigh_score, count = 0;
+        Uni last;
+        int curr_score = simulate();
+        int best_score = curr_score, new_score, delta, count = 0;
         double temp = INIT_TEMP, elapsed = 0.0;
         
         auto start = std::chrono::high_resolution_clock::now();
 
         do {
-            State neigh(curr);
-
-            neigh.make_change();
-            neigh_score = neigh.simulate();
-            delta = neigh_score - curr_score;
+            last = make_change();
+            new_score = simulate();
+            delta = new_score - curr_score;
 
             if (delta > 0 || std::exp(static_cast<double>(delta) / temp) > static_cast<double>(xorshift_rand()) / UINT32_MAX) {
-                curr = neigh;
-                curr_score = neigh_score;
+                curr_score = new_score;
 
                 if (curr_score > best_score) {
-                    best = curr;
+                    update_best();
                     best_score = curr_score;
                 }
+            }
+            else {
+                curr_board[last.x][last.y] = last.dir;
+                //print_board();
             }
 
             temp *= ALPHA;
@@ -283,23 +323,19 @@ private:
             }
         }
         while (elapsed < LIMIT);
+
         std::cerr << elapsed << " < " << LIMIT << std::endl;
         std::cerr << count << std::endl;
+        std::cerr << best_score << std::endl;
 
-        return best.get_new_arrows(this);
+        return get_new_arrows();
     }
  
 public:
-    State(const State *old_state = nullptr) {
-        if (old_state) {
-            std::memcpy(board, old_state->board, sizeof(board));
-        }
-    }
-
     void print_board() {
         for (int i = 0; i < HEIGHT; ++i) {
             for (int j = 0; j < WIDTH; ++j) {
-                std::cerr << board[i][j];
+                std::cerr << curr_board[i][j];
             }
         std::cerr << std::endl;
         }
@@ -338,7 +374,7 @@ public:
                 }
 
                 og_board[i][j] = cell;
-                board[i][j] = cell;
+                curr_board[i][j] = cell;
             }
         }
         
@@ -373,6 +409,8 @@ public:
     }
 
     void solve() {
+        start = std::chrono::high_resolution_clock::now();
+        preprocess();
         std::vector<Uni> res = best_sa();
         int size = res.size() - 1;
 
@@ -393,7 +431,7 @@ public:
                     std::cout << 'L';
                     break;
                 default:
-                    std::cout << "fail";
+                    std::cout << "FAIL";
                     break;
             }
 
